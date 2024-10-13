e3fdc1fcf2c983fce5fbad5dae1e3579a12ae04c
Wiraa-ui <i.kadek68363@smk.belajar.id>
2024-10-12 19:01:39 -0700

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+</head>
+<body>
+
+    <a href="/">Home</a>
+    <h1>Halaman About Wira</h1>
+    <h3>Owner: <?= $nama; ?></h3>
+
+    <img src="img/tes.jpg" alt="">
+
+</body>
+</html>
